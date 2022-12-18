6c56f42a8d82b6b551c4133e68f0f146e132d017
godofthunder8756 <ahernaidan1@gmail.com>
2022-12-17 23:19:20 -0500

    events "only happen once" support

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -5,64 +5,79 @@ import java.awt.Rectangle;
 public class EventHandler {
 	
 	GamePanel gp;
-	Rectangle eventRect;
-	int eventRectDefaultX, eventRectDefaultY;
+	EventRect eventRect[][];
 	
 	public EventHandler(GamePanel gp) {
 		this.gp = gp;
 		
-		eventRect = new Rectangle();
-		eventRect.x = 23;
-		eventRect.y = 23;
-		eventRect.width = 2;
-		eventRect.height = 2;
-		eventRectDefaultX = eventRect.x;
-		eventRectDefaultY = eventRect.y;
+		eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
+		
+		int col = 0;
+		int row = 0;
+		while(col < gp.maxWorldCol && row < gp.maxWorldRow) {
+			eventRect[col][row] = new EventRect();
+			eventRect[col][row].x = 23;
+			eventRect[col][row].y = 23;
+			eventRect[col][row].width = 2;
+			eventRect[col][row].height = 2;
+			eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+			eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+			
+			col++;
+			if(col == gp.maxWorldCol) {
+				col = 0;
+				row++;
+			}
+		}
+		
+		
 	}
 	
 	public void checkEvent() {
-//		if(hit(26, 16, "right") == true) { damagePit(gp.dialogueState); System.out.println("PIT");}
-		if(hit(26, 16, "right") == true) { teleport(gp.dialogueState); System.out.println("TELPORT");}
-		if(hit(23, 12, "up") == true) { healingPool(gp.dialogueState); System.out.println("HEALING");}
+//		if(hit(26, 16, "right") == true) { damagePit(gp.dialogueState);}
+		if(hit(26, 16, "right") == true) { teleport(26, 16, gp.dialogueState);}
+		if(hit(23, 12, "up") == true) { healingPool(23, 12, gp.dialogueState);}
 	}
 	
-	public boolean hit(int eventCol, int eventRow, String reqDirection) {
+	public boolean hit(int col, int row, String reqDirection) {
 		boolean hit = false;
 		
 		gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x; 
 		gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-		eventRect.x = eventCol*gp.tileSize + eventRect.x;
-		eventRect.y = eventRow*gp.tileSize + eventRect.y;
+		eventRect[col][row].x = col*gp.tileSize + eventRect[col][row].x;
+		eventRect[col][row].y = row*gp.tileSize + eventRect[col][row].y;
 		
-		if(gp.player.solidArea.intersects(eventRect)) {
+		if(gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
 			if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
 				hit = true;
 			}
 		}
 		gp.player.solidArea.x = gp.player.solidAreaDefaultX;
 		gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-		eventRect.x = eventRectDefaultX;
-		eventRect.y = eventRectDefaultY;
+		eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+		eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 		
 		return hit;
 	}
-	public void  teleport(int gameState) {
+	public void  teleport(int col, int row, int gameState) {
 		gp.gameState = gameState;
 		gp.ui.currentDialogue = "You have been \nTELEPORTED";
 		gp.player.worldX = gp.tileSize*37;
 		gp.player.worldY = gp.tileSize*10;
+		eventRect[col][row].eventDone = true;
 	}
-	public void damagePit(int gameState){
+	public void damagePit(int col, int row, int gameState){
 		gp.gameState = gameState;
 		gp.ui.currentDialogue = "You fell into a pit";
 		gp.player.life -= 1;
+		eventRect[col][row].eventDone = true;
 	}
-	public void healingPool(int gameState) {
+	public void healingPool(int col, int row, int gameState) {
 		if(gp.keyH.enterPressed == true) {
 			gp.gameState = gameState;
 			gp.ui.currentDialogue = "You drank from the secret \nhealing water...";
 			gp.player.life = gp.player.maxLife;
-			
+			eventRect[col][row].eventDone = true;
 		}
 		
 	}
