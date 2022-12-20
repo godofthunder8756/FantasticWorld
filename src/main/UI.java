a91e00035372e7400f17fcb69bf1fb96b160a556
godofthunder8756 <ahernaidan1@gmail.com>
2022-12-20 18:37:05 -0500

    Description frame now hides on null

--- src/main/UI.java
+++ src/main/UI.java
@@ -370,7 +370,8 @@ public class UI {
 		int dFrameY = frameY + frameHeight;
 		int dFrameWidth = frameWidth;
 		int dFrameHeight = gp.tileSize*3;
-		drawSubWindow(dFrameX, dFrameY, dFrameWidth, dFrameHeight);
+		
+		
 		// Draw Desiption text
 		int textX = dFrameX+20;
 		int textY = dFrameY+gp.tileSize;
@@ -379,6 +380,7 @@ public class UI {
 		int itemIndex = getItemIndexOnSlot();
 		
 		if(itemIndex < gp.player.inventory.size()) {
+			drawSubWindow(dFrameX, dFrameY, dFrameWidth, dFrameHeight);
 			for(String line: gp.player.inventory.get(itemIndex).description.split("\n")) {
 				g2.drawString(line, textX, textY);
 				textY += 32;
